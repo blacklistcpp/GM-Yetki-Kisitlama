9ecd7c5b7a8b0e26a092181ff822e5a389c54b48
blacklistcpp <72122797+blacklistcpp@users.noreply.github.com>
2021-03-07 00:43:24 +0300

    Create player.sql

---
+++ player.sql
@@ -0,0 +1,21 @@
+SET NAMES utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+
+-- Table structure for gamemaster_restriction
+
+-- ----------------------------
+
+DROP TABLE IF EXISTS `gamemaster_restriction`;
+
+CREATE TABLE `gamemaster_restriction` (
+
+`gamemaster` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+
+PRIMARY KEY (`gamemaster`) USING BTREE
+
+) ENGINE = MyISAM CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
